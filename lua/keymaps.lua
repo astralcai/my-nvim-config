ceae79e41738940de40b122426eb268649daf757
Astral Cai <astral.cai@xanadu.ai>
2025-08-04 19:31:31 -0400

    Options, Keymaps, and Lazy

---
+++ lua/keymaps.lua
@@ -0,0 +1,13 @@
+-- [[ Basic Keymaps ]]
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear highlights on search" })
+
+-- Keybinds to make split navigation easier.
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Mimic the vim-vinegar method of file navigation
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
---
+++ lua/options.lua
@@ -0,0 +1,83 @@
+-- [[ Basic Options ]]
+
+-- Set <space> as the leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Set Python location
+vim.g.python3_host_prog = "~/.pyenv/shims/python"
+
+-- Use nerd font
+vim.g.have_nerd_font = true
+
+-- Line numbers
+vim.o.number = true
+vim.o.relativenumber = true
+
+-- Wrapped lines have the same indentation
+vim.o.breakindent = true
+
+-- Highlight the line that the cursor is on
+vim.o.cursorline = true
+
+-- Use OS clipboard in Neovim.
+vim.o.clipboard = "unnamedplus"
+
+-- Ask confirmation to save current buffer before quitting
+vim.o.confirm = true
+
+-- Ignore case in search patterns
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Preview substitutions live
+vim.o.inccommand = "split"
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+vim.o.list = true
+vim.o.listchars = { tab = "» ", trail = "·" }
+
+-- Use OS clipboard for Neovim.
+vim.o.clipboard = "unnamedplus"
+
+-- Use spaces instead of tabs
+vim.o.expandtab = true
+
+-- Set default tab width
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+
+-- Enable mouse support
+vim.o.mouse = "a"
+
+-- Minimal number of lines to keep above and below the cursor
+vim.o.scrolloff = 10
+
+-- Do not show mode
+vim.o.showmode = false
+
+-- Keep signcolumn on
+vim.o.signcolumn = "yes"
+
+-- Default position of split windows
+vim.o.splitbelow = true
+vim.o.splitright = true
+
+-- Decrease update time
+vim.o.updatetime = 250
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Decrease mapped sequence wait time
+vim.o.timeoutlen = 300
+
+-- [[ Basic Autocommands]]
+
+-- Highlight yanked text.
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})

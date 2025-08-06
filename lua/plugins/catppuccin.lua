854ab95defa9e107a59afbea4ba64077bf1b99d7
Astral Cai <astral.cai@xanadu.ai>
2025-08-05 22:57:08 -0400

    Add Plugins

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,14 @@
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			styles = {
+				comments = {},
+				conditionals = {},
+			},
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
+}
---
+++ lua/plugins/whick-key.lua
@@ -0,0 +1,20 @@
+return {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		delay = 500,
+		spec = {
+			{ "<leader>s", group = "[S]earch" },
+			{ "<leader>t", group = "[T]oggle" },
+		},
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps",
+		},
+	},
+}

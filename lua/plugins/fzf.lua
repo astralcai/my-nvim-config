6b1ffb5338cfad233d4ba1215ce4502f28b48a8b
Astral Cai <astral.cai@xanadu.ai>
2025-10-22 10:46:05 -0400

    use live_grep vs live_grep_native

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -9,7 +9,7 @@ return {
     vim.keymap.set("n", "<leader>sh", fzf.help_tags, { desc = "[S]earch [H]elp" })
     vim.keymap.set("n", "<leader>sk", fzf.keymaps, { desc = "[S]earch [K]eymaps" })
     vim.keymap.set("n", "<leader>sf", fzf.files, { desc = "[S]earch [F]iles" })
-    vim.keymap.set("n", "<leader>sg", fzf.live_grep_native, { desc = "[S]earch by [G]rep" })
+    vim.keymap.set("n", "<leader>sg", fzf.live_grep, { desc = "[S]earch by [G]rep" })
     vim.keymap.set("n", "<leader>sd", fzf.diagnostics_document, { desc = "[S]earch [D]iagnostics" })
     vim.keymap.set("n", "<leader><leader>", fzf.buffers, { desc = "[ ] Current Buffers" })
     vim.keymap.set("n", "<leader>/", fzf.lgrep_curbuf, { desc = "[/] Search in Current Buffer" })

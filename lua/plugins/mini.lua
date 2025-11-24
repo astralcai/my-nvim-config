5c901f27041e13b600f39416cebb6bb5848335ea
Astral Cai <astralcai@gmail.com>
2025-11-23 22:32:34 -0500

    add scroll animation

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -5,5 +5,20 @@ return {
     require("mini.ai").setup({})
     require("mini.move").setup({})
     require("mini.comment").setup({})
+    local animate = require("mini.animate")
+    animate.setup({
+      cursor = { enable = false },
+      scroll = {
+        timing = animate.gen_timing.cubic({ easing = "out", duration = 50, unit = "total" }),
+        subscroll = animate.gen_subscroll.equal({
+          predicate = function(total_scroll)
+            return math.abs(total_scroll) > 5
+          end,
+        }),
+      },
+      resize = { enable = false },
+      open = { enable = false },
+      close = { enable = false },
+    })
   end,
 }

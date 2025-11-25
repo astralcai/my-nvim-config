8318695beb41b679645e67ccbf7284c52fc2de4a
Astral Cai <astral.cai@xanadu.ai>
2025-11-25 09:36:48 -0500

    project specific config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -12,14 +12,30 @@ return {
       desc = "[F]ormat Buffer",
     },
   },
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      python = { "ruff_organize_imports", "ruff_format" },
-      toml = { "tombi" },
-    },
-    default_format_opts = {
-      lsp_format = "fallback",
-    },
-  },
+  opts = function()
+    local global_opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "ruff_organize_imports", "ruff_format" },
+        toml = { "tombi" },
+      },
+      default_format_opts = {
+        lsp_format = "fallback",
+      },
+    }
+    local local_config_path = vim.fs.find({ ".conform.lua" }, {
+      path = vim.fn.getcwd(),
+      upward = true,
+      limit = 1,
+      stop = vim.loop.os_homedir(),
+    })[1]
+    if local_config_path then
+      if vim.loop.fs_stat(local_config_path) then
+        local f = assert(loadfile(local_config_path))
+        local local_opts = f()
+        return vim.tbl_deep_extend("force", global_opts, local_opts)
+      end
+    end
+    return global_opts
+  end,
 }
